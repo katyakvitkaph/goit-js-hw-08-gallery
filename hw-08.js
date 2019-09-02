b14f312e90015286e73f52247c2926cd8993b2da
katyakvitkaph <kvitka.katja@gmail.com>
2019-09-02 13:14:48 +0300

    .

--- hw-08.js
+++ hw-08.js
@@ -1,144 +1,103 @@
-'use script';
+"use script";
 
-import galleryL from "./gallery-items.js";
+import gallery from "./gallery-items.js";
 
-const createListItem = ({preview , original , description}) => {
-  const listItem = document.createElement('li');
+const refs = {
+  galleryUl: document.querySelector(".gallery"),
+  lightboxFirst: document.querySelector(".lightbox__overlay"),
+  divLightbox: document.querySelector("div.lightbox"),
+  lightboxImage: document.querySelector('.lightbox___image'),
+  closeModalBtn: document.querySelector('button[data-action="close-lightbox"]'),
+  liteboxOver: document.querySelector(".lightbox__content"),
+};
 
-listItem.classList.add('gallery__item');
-galleryUl.appendChild(listItem);
+const createListItem = ({ preview, original, description }) => {
+  const listItem = document.createElement("li");
 
-const pic = document.createElement('a');
-pic.classList.add("gallery__link");
+  listItem.classList.add("gallery__item");
+  refs.galleryUl.appendChild(listItem);
 
-pic.setAttribute("href", preview);
-listItem.appendChild(pic);
+  const pic = document.createElement("a");
+  pic.classList.add("gallery__link");
 
-const imgPrew = document.createElement('img');
-imgPrew.classList.add("gallery__image");
+  pic.setAttribute("href", preview);
+  listItem.appendChild(pic);
 
+  const imgPrew = document.createElement("img");
+  imgPrew.classList.add("gallery__image");
 
-imgPrew.setAttribute("src", original);
-imgPrew.setAttribute("alt", description);
-imgPrew.dataset.source = original;
+  imgPrew.setAttribute("src", original);
+  imgPrew.setAttribute("alt", description);
+  imgPrew.dataset.source = original;
 
-const icon = document.createElement('span');
-icon.classList.add("gallery__icon");
-pic.appendChild(imgPrew, icon);
+  const icon = document.createElement("span");
+  icon.classList.add("gallery__icon");
+  pic.append(imgPrew, icon);
+  // pic.appendChild(icon);
 
-const iIcon = document.createElement('i');
-iIcon.classList.add("material-icons");
-iIcon.textContent = 'zoom_out_map';
-icon.appendChild(iIcon);
+  const iIcon = document.createElement("i");
+  iIcon.classList.add("material-icons");
+  iIcon.textContent = "zoom_out_map";
+  icon.appendChild(iIcon);
 
-return listItem;
-}
+  return listItem;
+};
 
 const renderListItems = (listRef, gallery) => {
-  const listItems = galleryL.map(item => createListItem(item));
-  listRef.append(...listItems  );
-}
+  const listItems = gallery.map(item => createListItem(item));
+  listRef.append(...listItems);
+};
 
-// const but = document.createElement('button');
-// but.classList.add('lightbox__button');
-// but.dataset.action = 'close-lightbox';
-// but.setAttribute('type', 'button');
-// but.appendChild()
-
-
-
-
-const galleryUl = document.querySelector('.gallery');
-
-
-
-renderListItems(galleryUl, galleryL);
+renderListItems(refs.galleryUl, gallery);
 
 // MODAL WINDOW =========
 
+// event.preventDefault();
 
-const openModal = document.querySelector('.gallery');
-const closeModal = document.querySelector('.lightbox__overlay');
-
-
-openModal.addEventListener('click', openM);
-closeModal.addEventListener('click', closeM);
-
-function openM() {
-  document.body.classList.add('show-model');
-}
-
-
-function closeM() {
-  document.body.classList.remove('show-model');
-}
-
-
-
-
-
-
-
-
-// ================================copy 
-// const listItem = document.createElement('li');
-
-// listItem.classList.add('gallery__item');
-// galleryUl.appendChild(listItem);
-
-// const pic = document.createElement('a');
-// pic.classList.add("gallery__link");
-
-// pic.setAttribute("href", "https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg");
-// listItem.appendChild(pic);
-
-// const imgPrew = document.createElement('img');
-// imgPrew.classList.add("gallery__image");
-// pic.appendChild(imgPrew);
-
-// imgPrew.setAttribute("src", "https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg");
-// imgPrew.setAttribute("alt", "Hokkaido Flower");
-// imgPrew.dataset.source = ('https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825_1280.jpg')
-
-// const icon = document.createElement('span');
-// icon.classList.add("gallery__icon");
-// pic.appendChild(icon);
-
-// const iIcon = document.createElement('i');
-// iIcon.classList.add("material-icons");
-// iIcon.textContent = 'zoom_out_map';
-// icon.appendChild(iIcon);
-
-// ===============end of copy
-
-// function createHtnml(galleryShow) {
-//   const { preview, original, description } = galleryShow; // деструктуризация объекта
-//   const prew = preview
-//     .map((pic, i) => {
-//       const mainHtml =`<li class="gallery__item">
-//       <a
-//         class="gallery__link"
-//         href="${pic.preview}"
-//       >
-//         <img
-//           class="gallery__image"
-//           src="${pic.preview}"
-//           data-source="${original}"
-//           alt="Tulips"
-//         />
-    
-//         <span class="gallery__icon">
-//           <i class="material-icons">zoom_out_map</i>
-//         </span>
-//       </a>
-//     </li>
-// `; 
-//       return mainHtml;
-//     })
-//     .join(""); // это обьедение всех элементов массива в строку
-//   const mainTitle = `<h1>${description}</h1>`; // создание разметки тайтла
-
-//   return mainTitle + prew; // возвращаем результат ф-и , конкатенация элементов
+// if (event.target !== event.currentTarget) {
+// refs.lightboxFirst.classList.add("is-open");
 // }
-// createHtnml(gallery);
 
+refs.galleryUl.addEventListener("click", openModalTwo);
+refs.closeModalBtn.addEventListener("click", closeModal);
+refs.liteboxOver.addEventListener('click', handleOverlay);
+// function openModal () {
+//   event.preventDefault();
+//   document.body.classList.add('.show-modal');
+// }
+
+function openModalTwo(event) {
+  if (event.target !== event.currentTarget) {
+  event.preventDefault();
+  refs.divLightbox.classList.add("is-open");
+  const altImg = event.target.alt;
+  const dataImg = event.target.dataset.source;
+  refs.lightboxImage.src = dataImg;
+  refs.lightboxImage.setAttribute('alt', altImg);
+  window.addEventListener('keydown' , handleKeyPress );
+  
+  
+  }
+}
+
+function closeModal () {
+  refs.divLightbox.classList.remove("is-open");
+  window.removeEventListener('keydown' , handleKeyPress);
+}
+
+function handleOverlay (event) {
+  if (event.target !== event.currentTarget) {
+    // console.log("target----", event.target);
+    // console.log("currentTarget----", event.currentTarget);
+   return;
+    }
+  
+    closeModal();
+}
+
+function handleKeyPress (event) {
+  if (event.code !== 'Escape') {
+    return;
+  }
+  closeModal();
+}
